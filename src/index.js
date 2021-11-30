9042fab822782e0fca4476a6707b6b354643e5d8
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-11-30 15:38:00 -0600

    added click event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './less/index.less'
 
 // Your code goes here!
+const btn = document.querySelectorAll('.btn'); 
+const letsGoP = document.querySelector('p'); 
+
+
+//click 
+document.addEventListener('click', e => {
+    console.log(e)
+    e.target.style.backgroundColor = 'pink'; 
+}); 
+
+//select 
+letsGoP.addEventListener('select', event => {
+    event.target.value.substring
+})
+
+//focus 
+
+
+
+
+
+//blur 
