835d166381c8e0b0c2897c90ce156738c6ff34d7
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-11-30 16:27:22 -0600

    added mouseover and mouse out

--- src/index.js
+++ src/index.js
@@ -1,25 +1,64 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//click
+const divs = document.querySelectorAll('div')
+
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+        console.log('hi')
+    })
+})
+
+//const btn = document.querySelector('.btn')
+
+//btn.addEventListener('click', e => {
+   // e.target.style.backgroundColor = 'red'
+//})
+
+
 const btn = document.querySelectorAll('.btn'); 
+btn.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        e.target.style.backgroundColor= 'yellow'
+    })
+})
+
+
+//mouse over 
+const logoHead = document.querySelector('.logo-heading'); 
+const mousePurple = () => {
+    logoHead.style.background = 'purple'; 
+}
+logoHead.addEventListener('mouseover', mousePurple)
+
+const mouseOut = () => {
+    logoHead.style.backgroundColor = ''; 
+}
+logoHead.addEventListener('mouseout', mouseOut); 
+
+
+
+
+
+
+/*
+ 
+const subtitle = document.querySelector('.into h2'); 
 const letsGoP = document.querySelector('p'); 
 
-
-//click 
-document.addEventListener('click', e => {
-    console.log(e)
-    e.target.style.backgroundColor = 'pink'; 
-}); 
 
 //select 
 letsGoP.addEventListener('select', event => {
     event.target.value.substring
 })
 
-//focus 
-
-
-
-
-
-//blur 
+//keydown
+function subtitlePurp(e) {
+    if (e.key === 'Enter') {
+        subtitle.style.color = '#9932CC'; 
+    }
+}
+document.addEventListener('keydown', subtitlePurp); 
+*/
